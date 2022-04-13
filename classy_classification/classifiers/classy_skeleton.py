72ebd78bc4d080de49ef2db4562f0b75607d78a2
David Berenstein <david.berenstein@pandoraintelligence.com>
2022-04-13 16:53:46 +0200

    updated formatting

--- classy_classification/classifiers/classy_skeleton.py
+++ classy_classification/classifiers/classy_skeleton.py
@@ -1,5 +1,4 @@
-from re import L
-from typing import Dict, List
+from typing import List
 
 import numpy as np
 from sklearn import preprocessing
@@ -8,14 +7,11 @@ from sklearn.svm import SVC
 
 
 class classySkeleton(object):
-    def __init__(self, 
-            data: dict, 
-            config: dict = {                              
-                "C": [1, 2, 5, 10, 20, 100],
-                "kernels": ["linear"],                              
-                "max_cross_validation_folds": 5
-            }
-        ) -> None:
+    def __init__(
+        self,
+        data: dict,
+        config: dict = {"C": [1, 2, 5, 10, 20, 100], "kernels": ["linear"], "max_cross_validation_folds": 5},
+    ) -> None:
         """initialize a classy skeleton for classification using a SVC config and some input training data.
 
         Args:
@@ -25,17 +21,17 @@ class classySkeleton(object):
                     "class 2": ["example"]
                 },
             device (str): device "cuda"/"cpu",
-            config (_type_, optional): a SVC config. 
-                Defaults to 
-                { 
-                    "C": [1, 2, 5, 10, 20, 100], 
-                    "kernels": ["linear"], 
-                    "max_cross_validation_folds": 5 
+            config (_type_, optional): a SVC config.
+                Defaults to
+                {
+                    "C": [1, 2, 5, 10, 20, 100],
+                    "kernels": ["linear"],
+                    "max_cross_validation_folds": 5
                 }.
         """
         self.data = data
-        self.config = config 
-        
+        self.config = config
+
     def __call__(self, text: str) -> dict:
         """predict the class for an input text
 
@@ -47,9 +43,9 @@ class classySkeleton(object):
         """
         embeddings = self.get_embeddings(text)
         embeddings = embeddings.reshape(1, -1)
-                
+
         return self.get_prediction(embeddings)[0]
-    
+
     def pipe(self, text: List[str]) -> List[dict]:
         """retrieve predicitons for multiple texts
 
@@ -60,9 +56,9 @@ class classySkeleton(object):
             List[dict]: list of key-class proba-value dict
         """
         embeddings = self.get_embeddings(text)
-        
+
         return self.get_prediction(embeddings)
-    
+
     def get_prediction(self, embeddings: List[List]) -> List[dict]:
         """get the predicitons for a list om embeddings
 
@@ -73,9 +69,9 @@ class classySkeleton(object):
             List[dict]: list of key-class proba-value dict
         """
         pred_result = self.clf.predict_proba(embeddings)
-        
+
         return self.proba_to_dict(pred_result)
-    
+
     def proba_to_dict(self, pred_results: List[List]) -> List[dict]:
         """converts probability prediciton to a formatted key-class proba-value list
 
@@ -88,62 +84,58 @@ class classySkeleton(object):
 
         pred_dict = []
         for pred in pred_results:
-            pred_dict.append(
-                {label: value for label, value in zip(self.le.classes_, pred)}
-            )
-            
+            pred_dict.append({label: value for label, value in zip(self.le.classes_, pred)})
+
         return pred_dict
-    
+
     def get_embeddings(self, _):
-        """ is overwritten by a superclass to retrieve embeddings """
+        """is overwritten by a superclass to retrieve embeddings"""
         pass
-    
+
     def set_training_data(self, data: dict = None):
         """_summary_
 
         Args:
-            data (dict, optional): a dict containing category keys and lists ov example values. 
+            data (dict, optional): a dict containing category keys and lists ov example values.
                 Defaults to None if self.data needs to be used.
         """
-        if data: # update if overwritten
+        if data:  # update if overwritten
             self.data = data
-            
+
         self.le = preprocessing.LabelEncoder()
         labels = []
         X = []
         self.label_list = list(self.data.keys())
         for key, value in self.data.items():
-            labels += len(value)*[key]
+            labels += len(value) * [key]
             X += value
         self.y = self.le.fit_transform(labels)
         self.X = self.get_embeddings(X)
-        
-        if data: # update if overwritten
+
+        if data:  # update if overwritten
             self.set_svc()
-    
-          
+
     def set_svc(self, config: dict = None):
         """Set and fit the SVC model.
 
         Args:
-            config (dict, optional): A config containing keys for SVC kernels, C, max_cross_validation_folds. 
+            config (dict, optional): A config containing keys for SVC kernels, C, max_cross_validation_folds.
                 Defaults to None if self.config needs to be used.
         """
-        if config: # update if overwritten
+        if config:  # update if overwritten
             self.config = config
-            
-        C = self.config["C"]          
+
+        C = self.config["C"]
         kernels = self.config["kernels"]
-        tuned_parameters = [{"C": C, "kernel": [str(k) for k in kernels]}]                                                                 
-        folds = self.config["max_cross_validation_folds"]                       
+        tuned_parameters = [{"C": C, "kernel": [str(k) for k in kernels]}]
+        folds = self.config["max_cross_validation_folds"]
         cv_splits = max(2, min(folds, np.min(np.bincount(self.y)) // 5))
         self.clf = GridSearchCV(
-            SVC(C=1, probability=True, class_weight='balanced'),
-            param_grid=tuned_parameters, 
-            n_jobs=1, 
-            cv=cv_splits,         
-            scoring='f1_weighted', 
-            verbose=1
+            SVC(C=1, probability=True, class_weight="balanced"),
+            param_grid=tuned_parameters,
+            n_jobs=1,
+            cv=cv_splits,
+            scoring="f1_weighted",
+            verbose=1,
         )
         self.clf.fit(self.X, self.y)
-        
--- classy_classification/classifiers/sentence_transformer.py
+++ classy_classification/classifiers/sentence_transformer.py
@@ -6,40 +6,37 @@ from .classy_skeleton import classySkeleton
 
 
 class classySentenceTransformer(classySkeleton):
-    def __init__(self, 
-            data: dict, 
-            model: str = 'sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2', 
-            device: str = 'cpu',
-            config: dict={                              
-                "C": [1, 2, 5, 10, 20, 100],
-                "kernels": ["linear"],                              
-                "max_cross_validation_folds": 5
-            },
-        ):
+    def __init__(
+        self,
+        data: dict,
+        model: str = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+        device: str = "cpu",
+        config: dict = {"C": [1, 2, 5, 10, 20, 100], "kernels": ["linear"], "max_cross_validation_folds": 5},
+    ):
         super().__init__(data=data, config=config)
         self.model = model
         self.device = device
         self.set_embedding_model()
         self.set_training_data()
         self.set_svc()
-        
-    def set_embedding_model(self, model: str = None, device: str = 'cpu'):
-        """ set the embedding model based on a sentencetransformer model or path
+
+    def set_embedding_model(self, model: str = None, device: str = "cpu"):
+        """set the embedding model based on a sentencetransformer model or path
 
         Args:
             model (str, optional): the model name. Defaults to self.model, if no model is provided.
         """
-        if model: # update if overwritten
+        if model:  # update if overwritten
             self.model = model
         if device:
             self.device = device
-            
+
         self.embedding_model = SentenceTransformer(self.model, device=self.device)
-        
-        if model: # update if overwritten
+
+        if model:  # update if overwritten
             self.set_training_data()
-            self.set_svc()  
-            
+            self.set_svc()
+
     def get_embeddings(self, X: List[str]) -> List[List[float]]:
         """retrieve embeddings from the SentenceTransformer model for a text or list of texts
 
@@ -50,11 +47,3 @@ class classySentenceTransformer(classySkeleton):
             List[List[float]]: output embeddings
         """
         return self.embedding_model.encode(X)
-    
-    
-    
-    
-    
-        
-    
-    
