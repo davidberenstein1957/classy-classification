30bd3be4a01be802de0c4f5a1a7745fb08db9eb6
Robin Rojowiec <rojowiec@hotmail.com>
2023-12-01 00:10:49 +0100

    formatted with black

--- classy_classification/examples/spacy_few_shot_external.py
+++ classy_classification/examples/spacy_few_shot_external.py
@@ -10,5 +10,5 @@ print([sent._.cats for sent in nlp(validation_data[0]).sents])
 print([doc._.cats for doc in nlp.pipe(validation_data)])
 
 nlp = spacy.blank("en")
-nlp.add_pipe("classy_classification", config={"data": training_data, "include_doc": False,"include_span": True})
+nlp.add_pipe("classy_classification", config={"data": training_data, "include_doc": False, "include_span": True})
 print([doc._.cats for doc in nlp.pipe(validation_data)])
